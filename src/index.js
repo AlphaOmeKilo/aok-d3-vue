56359155c622ce31a0c1ad1c89b5647c4c476706
Alex O'Keeffe <alexwokeeffe@gmail.com>
2020-07-31 14:16:52 +0100

    export line

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as D3XAxisLinear } from './components/D3XAxisLinear.vue';
 export { default as D3YAxisLinear } from './components/D3YAxisLinear.vue';
 export { default as D3XAxisLabel } from './components/D3XAxisLabel.vue';
 export { default as D3YAxisLabel } from './components/D3YAxisLabel.vue';
+export { default as D3Line } from './components/D3Line.vue';
